96be1983953d5b5455c049d96de05d0e94aca227
naiko <3443471283@qq.com>
2025-10-07 21:39:43 +0800

    test:替换astro为react

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,16 +4,19 @@ const navLinks = [
   { id: 'home', label: '首页',path:'/'},
   { id: 'home', label: '关于',path:'/about/'},
   { id: 'home', label: '博客',path:'/blog/'},
-   { id: 'home', label: '博客',path:'/tags/'},
+   { id: 'home', label: '标签',path:'/tags/'},
 ]
 
 function Navigation(){
   return (
     <div className="nav-links">
-      <a href="/">首页</a>
-      <a href="/about/">关于</a>
-      <a href="/blog/">博客</a>
-      <a href="/tags/">标签</a>
+     {
+      navLinks.map(i =>(
+        <a href={i.path} key={i.id}>
+          {i.label}
+        </a>
+      ))
+     }
     </div>
   );
 }
